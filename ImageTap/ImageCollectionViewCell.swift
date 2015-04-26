391aa5a9643745a1768cee4998977b43424da4e4
Jay Zheng <jay.j.zheng@gmail.com>
2015-04-26 14:47:10 -0700

    initial ImageCollectionView with grid of numbers

---
+++ ImageTap/ImageCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCollectionViewCell.swift
+//  ImageTap
+//
+//  Created by Jay Zheng on 4/26/15.
+//  Copyright (c) 2015 Jay Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+}
