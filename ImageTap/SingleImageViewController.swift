740f790e65a13807d8a1b0e8b64f7a5c7a3de312
Jay Zheng <jay.j.zheng@gmail.com>
2015-05-04 22:49:44 -0700

    initial recorder

--- ImageTap/SingleImageViewController.swift
+++ ImageTap/SingleImageViewController.swift
@@ -7,14 +7,114 @@
 //
 
 import UIKit
+import AVFoundation
 
-class SingleImageViewController: UIViewController {
+class SingleImageViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
     
     @IBOutlet weak var imageView: UIImageView!
-    var imageName: String? = "dog.jpg"
+    
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    var imageName: String? = ""
+    
+    var audioPlayer: AVAudioPlayer?
+    var audioRecorder: AVAudioRecorder?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = UIImage(named: imageName!)
+        
+        setupRecorder()
+        stopButton.enabled = false
+        playButton.enabled = false
     }
+    
+    @IBAction func startRecording(sender: UIButton) {
+        if audioRecorder?.recording == false {
+            playButton.enabled = false
+            stopButton.enabled = true
+            
+            audioRecorder?.record()
+        }
+    }
+    
+    @IBAction func stopRecording(sender: UIButton) {
+        stopButton.enabled = false
+        playButton.enabled = true
+        recordButton.enabled = true
+        
+        if audioRecorder?.recording == true {
+            audioRecorder?.stop()
+        } else {
+            audioPlayer?.stop()
+        }
+    }
+    
+    @IBAction func playRecording(sender: UIButton) {
+        if audioRecorder?.recording == false {
+            stopButton.enabled = true
+            recordButton.enabled = false
+            
+            var error: NSError?
+            
+            audioPlayer = AVAudioPlayer(contentsOfURL: audioRecorder?.url,
+                error: &error)
+            
+            audioPlayer?.delegate = self
+            
+            if let err = error {
+                println("audioPlayer error: \(err.localizedDescription)")
+            } else {
+                audioPlayer?.play()
+            }
+        }
+    }
+    
+    private func setupRecorder() {
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let docsDir = dirPaths[0] as! String
+        let soundFilePath = docsDir.stringByAppendingPathComponent("sound.caf")
+        let soundFileURL = NSURL(fileURLWithPath: soundFilePath)
+        let recordSettings = [AVEncoderAudioQualityKey: AVAudioQuality.Min.rawValue,
+                              AVEncoderBitRateKey: 16,
+                              AVNumberOfChannelsKey: 2,
+                              AVSampleRateKey: 44100.0]
+        
+        var error: NSError?
+        
+        let audioSession = AVAudioSession.sharedInstance()
+        audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord, error: &error)
+        
+        if let err = error {
+            println("audioSession error: \(err.localizedDescription)")
+        }
+        
+        audioRecorder = AVAudioRecorder(URL: soundFileURL, settings: recordSettings as [NSObject : AnyObject], error: &error)
+        
+        if let err = error {
+            println("audioSession error: \(err.localizedDescription)")
+        } else {
+            audioRecorder?.prepareToRecord()
+        }
+    }
+    
+    // MARK Delegations
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer!, successfully flag: Bool) {
+        recordButton.enabled = true
+        stopButton.enabled = false
+    }
+    
+    func audioPlayerDecodeErrorDidOccur(player: AVAudioPlayer!, error: NSError!) {
+        println("Audio Play Decode Error")
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder!, successfully flag: Bool) {
+    }
+    
+    func audioRecorderEncodeErrorDidOccur(recorder: AVAudioRecorder!, error: NSError!) {
+        println("Audio Record Encode Error")
+    }
+    
 }
